513396bb6f134349519e558d29ed3b6852461421
HUBId <31565807+HUBId@users.noreply.github.com>
2025-10-16 13:37:09 +0200

    Import malachite-float 0.4.18 core modules

---
+++ vendor/malachite-float/0.4.18/src/arithmetic/mod.rs
@@ -0,0 +1,16 @@
+// Copyright © 2025 Mikhail Hogrefe
+//
+// This file is part of Malachite.
+//
+// Malachite is free software: you can redistribute it and/or modify it under the terms of the GNU
+// Lesser General Public License (LGPL) as published by the Free Software Foundation; either version
+// 3 of the License, or (at your option) any later version. See <https://www.gnu.org/licenses/>.
+
+/// Addition of [`Float`](super::Float)s, and of [`Float`](super::Float)s with
+/// [`Rational`](malachite_q::Rational)s.
+pub mod add;
+/// Negation of [`Float`](super::Float)s.
+pub mod neg;
+/// Subtraction of [`Float`](super::Float)s, and of [`Float`](super::Float)s with
+/// [`Rational`](malachite_q::Rational)s.
+pub mod sub;
